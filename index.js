93a7b28789c17ac3c0717684cab10b1748e88920
antar232 <dasantar451@gmail.com>
2026-05-17 23:55:44 +0600

    Add a MongoDB server

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const uri = "mongodb+srv://MediQueue:8XJpcnyAWUcw9pzp@cluster0.zlrikti.mongodb.net/?appName=cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);

79066b349857400cd2732bad08f8e664a7a76fa6
antar232 <dasantar451@gmail.com>
2026-05-21 16:36:50 +0600

    Edit cross part

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const app = express();
 //app.use(cors());
 app.use(cors({
   origin: `${process.env.CLIENT_URL}`,
-  credentials: true
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
 }));
 app.use(express.json());
 
